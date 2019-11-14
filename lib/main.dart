b00aad40982de777b1f57920749686f75d54e0cd
Josef Wilhelm <3feetsmaller88@gmail.com>
2019-11-14 19:14:08 +0800

    bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_website/screens/base.dart';
 import 'package:flutter_website/service_locator.dart';
 import 'package:flutter_website/services/color_service.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   setupLocator();
